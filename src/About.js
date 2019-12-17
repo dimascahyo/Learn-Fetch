23e8f53d3ed20ed2f27e715fb2c679916430ce54
Dimas Cahyo Utomo <dimas_cahyo_26rpl@student.smktelkom-mlg.sch.id>
2019-12-17 15:44:44 +0700

    Belajar Fetch Data

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class About extends Component{
+    render(){
+        return(
+            <div>
+                <h2>About Random User</h2>
+                <p>RANDOM USER GENERATOR , dummy API for generating random user data. Like Lorem Ipsum, but for people. I used this to learn fetch data in react.</p>
+            </div>
+        );
+    }
+}
+
+export default About;
